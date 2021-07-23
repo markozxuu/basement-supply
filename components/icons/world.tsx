6871f41d690a23c24d7224cb7a36776891136448
Markoz Pena <markdrew53@gmail.com>
2021-07-23 17:29:56 -0500

    feat: Added world svg icon

---
+++ components/icons/world.tsx
@@ -0,0 +1,32 @@
+const World = () => (
+  <svg width="34" height="34" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M17 33c8.837 0 16-7.163 16-16S25.837 1 17 1 1 8.163 1 17s7.163 16 16 16z"
+      stroke="#fff"
+      strokeMiterlimit="10"
+    />
+    <path
+      d="M17 33c5.824 0 10.545-7.163 10.545-16S22.824 1 17 1 6.455 8.163 6.455 17 11.176 33 17 33z"
+      stroke="#fff"
+      strokeMiterlimit="10"
+    />
+    <path
+      d="M17 33c2.811 0 5.09-7.163 5.09-16S19.811 1 17 1c-2.812 0-5.09 7.163-5.09 16S14.187 33 17 33z"
+      stroke="#fff"
+      strokeMiterlimit="10"
+    />
+    <path
+      d="M1 16.898h32M2.15 11.043h29.707M6.207 5.195h21.586M6.105 28.71h21.797M2.107 22.855h29.786"
+      stroke="#fff"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+    />
+    <path
+      d="M28.805 17.058c-9.642-.043-11.82 2.112-11.863 11.747.043-9.642-2.112-11.82-11.747-11.863 9.642.043 11.82-2.112 11.863-11.747-.043 9.635 2.112 11.812 11.747 11.863z"
+      fill="#fff"
+    />
+    <path d="M17 1v31.993" stroke="#fff" strokeMiterlimit="10" />
+  </svg>
+);
+
+export default World;
